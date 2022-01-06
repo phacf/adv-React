d2c19cf15c5f50d9b65c9fe43d88e7f2a50065b4
Paulo <phacff@gmail.com>
2022-01-06 17:17:59 -0300

    tests configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,44 +1,40 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:prettier/recommended",
-        "prettier"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint",
-        "react-hooks",
-        "prettier"
-    ],
-    "rules": {
-        "react-hooks/rules-of-hooks": "error",
-        "react-hooks/exhaustive-deps": "warn",
-        "react/prop-types": "off",
-        "react/react-in-jsx-scope": "off",
-        "prettier/prettier": "error",
-        "@typescript-eslint/explicit-module-boundary-types": "off",
-        "arrow-body-style": "off",
-        "prefer-arrow-callback": "off"        
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  settings: {
+    react: {
+      version: 'detect'
     }
-};
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+    'prettier'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 13,
+    sourceType: 'module'
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'arrow-body-style': 'off',
+    'prefer-arrow-callback': 'off'
+  }
+}
---
+++ src/Components/Main/index.tsx
@@ -0,0 +1,8 @@
+const Main = () => {
+  return (
+    <main>
+      <h1>React Avançado</h1>
+    </main>
+  )
+}
+export default Main
