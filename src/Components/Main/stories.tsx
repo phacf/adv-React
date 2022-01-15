9a27a414cbe89007d688c8e43c185dfacf215de4
Paulo <phacff@gmail.com>
2022-01-15 11:02:56 -0300

    lint

--- src/Components/Main/stories.tsx
+++ src/Components/Main/stories.tsx
@@ -1,11 +1,13 @@
 import Main from '.'
 
-import { withKnobs, text, boolean, number } from '@storybook/addon-knobs';
-import {Story, Meta} from "@storybook/react/types-7-0";
+import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
+import { Story, Meta } from '@storybook/react/types-7-0'
 export default {
-    title: 'Main',
-    component: Main,
-    decorators: [withKnobs]
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
 } as Meta
 
-export const Basic: Story = () => <Main title={text('title')} description={text('description')}/>
+export const Basic: Story = () => (
+  <Main title={text('title')} description={text('description')} />
+)
