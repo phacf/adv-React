b961e2908ded64c8c528dfddd952da130aed6c6f
Paulo <phacff@gmail.com>
2022-01-18 17:45:18 -0300

    add pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,13 @@ function App({ Component, pageProps }: AppProps) {
         <title>React-avançado-boilerplate</title>
         <link rel="shortcut icon" href="../public/img/download.png" />
         <link rel="apple-touch-icon" href="../public/img/download.png" />
-        <meta name="description" content="A simple project starter to work with typescript, react, next and styled components" />
+        <link rel="manifest" href="manifest.json" />
+        <meta
+          name="description"
+          content="A simple project starter to work with typescript, react, next and styled components"
+        />
       </Head>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Component {...pageProps} />
     </>
   )
